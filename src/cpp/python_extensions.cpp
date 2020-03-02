9b1f2694f00aea01a6f479e5e56539a34318e50b
Arpit Aggarwal <ar13pit@gmail.com>
2020-03-02 02:54:22 +0100

    fix: Fix type conversion issues from python
    Updated the wrapper interface to account for type conversions

--- src/cpp/python_extensions.cpp
+++ src/cpp/python_extensions.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include <pybind11/pybind11.h>
 #include <string>
 #include "gmm_wrappers.h"
@@ -25,7 +26,21 @@ PYBIND11_MODULE(_Extensions, m)
 
   py::class_<kaldi::NNet3OnlineDecoderWrapper>(m, "NNet3OnlineDecoderWrapper")
       .def(py::init<kaldi::NNet3OnlineModelWrapper *>())
-      .def("decode", &kaldi::NNet3OnlineDecoderWrapper::decode)
-      .def("get_decoded_string", &kaldi::NNet3OnlineDecoderWrapper::get_decoded_string)
+      .def("decode",
+           [](kaldi::NNet3OnlineDecoderWrapper &m, float samp_freq, py::buffer frames_buffer, bool finalize) {
+             py::buffer_info info = frames_buffer.request();
+             if (info.ndim != 1)
+             {
+               throw std::runtime_error("Incompatible buffer dimensions");
+             }
+
+             return m.decode(samp_freq, info.shape[0], static_cast<float *>(info.ptr), finalize);
+           })
+      .def("get_decoded_string",
+           [](kaldi::NNet3OnlineDecoderWrapper &m, double likelihood) {
+             std::string decoded_string = "";
+             m.get_decoded_string(decoded_string, likelihood);
+             return std::tuple<std::string, double>(decoded_string, likelihood);
+           })
       .def("get_word_alignment", &kaldi::NNet3OnlineDecoderWrapper::get_word_alignment);
 }
