40f8ba1968cc7d6ae73e8f51d94d61fb88703ac5
Arpit Aggarwal <ar13pit@gmail.com>
2020-03-02 03:02:15 +0100

    fix: Apply nnet3 fix to gmm wrappers

--- src/cpp/python_extensions.cpp
+++ src/cpp/python_extensions.cpp
@@ -8,22 +8,44 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(_Extensions, m)
 {
-  // gmm_wrappers
+  /*
+   * gmm_wrappers
+   */
+  // GMM Online Model Wrapper
   py::class_<kaldi::GmmOnlineModelWrapper>(m, "GmmOnlineModelWrapper")
       .def(py::init<float, int, int, float, std::string &, std::string &, std::string &,
                     std::string &>());
 
+  // GMM Online Decoder Wrapper
   py::class_<kaldi::GmmOnlineDecoderWrapper>(m, "GmmOnlineDecoderWrapper")
       .def(py::init<kaldi::GmmOnlineModelWrapper *>())
-      .def("decode", &kaldi::GmmOnlineDecoderWrapper::decode)
-      .def("get_decoded_string", &kaldi::GmmOnlineDecoderWrapper::get_decoded_string)
+      .def("decode",
+           [](kaldi::GmmOnlineDecoderWrapper &m, float samp_freq, py::buffer frames_buffer, bool finalize) {
+             py::buffer_info info = frames_buffer.request();
+             if (info.ndim != 1)
+             {
+               throw std::runtime_error("Incompatible buffer dimensions");
+             }
+
+             return m.decode(samp_freq, info.shape[0], static_cast<float *>(info.ptr), finalize);
+           })
+      .def("get_decoded_string",
+           [](kaldi::GmmOnlineDecoderWrapper &m, double likelihood) {
+             std::string decoded_string = "";
+             m.get_decoded_string(decoded_string, likelihood);
+             return std::tuple<std::string, double>(decoded_string, likelihood);
+           })
       .def("get_word_alignment", &kaldi::GmmOnlineDecoderWrapper::get_word_alignment);
 
-  // nnet3_wrappers
+  /*
+   * nnet3_wrappers
+   */
+  // NNet3 Online Model Wrapper
   py::class_<kaldi::NNet3OnlineModelWrapper>(m, "NNet3OnlineModelWrapper")
       .def(py::init<float, int, int, float, float, int, std::string &, std::string &, std::string &,
                     std::string &, std::string &, std::string &>());
 
+  // NNet3 Online Decoder Wrapper
   py::class_<kaldi::NNet3OnlineDecoderWrapper>(m, "NNet3OnlineDecoderWrapper")
       .def(py::init<kaldi::NNet3OnlineModelWrapper *>())
       .def("decode",
