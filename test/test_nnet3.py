c86fc485c8a26004923a2af5630a1457e313f4f4
Arpit Aggarwal <ar13pit@gmail.com>
2020-03-03 21:06:06 +0100

    test: Add test to read wav file in chunks

--- test/test_nnet3.py
+++ test/test_nnet3.py
@@ -1,3 +1,7 @@
+import math
+import struct
+import wave
+import numpy as np
 from yapykaldi import KaldiNNet3OnlineDecoder, KaldiNNet3OnlineModel
 
 model_dir = "../data/kaldi-generic-en-tdnn_fl-latest"
@@ -6,20 +10,63 @@ wavfile = "../data/lsen1.wav"
 model = KaldiNNet3OnlineModel(model_dir)
 decoder = KaldiNNet3OnlineDecoder(model)
 
+# Test 1 Read the whole wav file
+print()
+print("*****************************************************************")
+print("Test 1: Read the whole .wav file")
+print("*****************************************************************")
+print()
 if decoder.decode_wav_file(wavfile):
     decoded_string, likelihood = decoder.get_decoded_string()
     words, times, lengths = decoder.get_word_alignment()
 
-    print()
-    print("*****************************************************************")
     print("**", wavfile)
     print("**", decoded_string)
     print("** {} likelihood:".format(model_dir), likelihood)
     print("** Word alignment: {}".format(words))
     print("** Time alignment: {}".format(times))
     print("** Length alignment: {}".format(lengths))
-    print("*****************************************************************")
-    print()
 
 else:
     print("***ERROR: decoding of {} failed.".format(wavfile))
+print("*****************************************************************")
+print()
+
+# Test 2 Read the wav file in chunks
+print()
+print("*****************************************************************")
+print("Test 2: Read the .wav file in chunks")
+print("*****************************************************************")
+print()
+print("**", wavfile)
+
+CHUNK = 1024
+try:
+    with wave.open(wavfile, 'rb') as wavf:
+        assert wavf.getnchannels() == 1
+        assert wavf.getsampwidth() == 2
+        assert wavf.getnframes() > 0
+
+        frame_rate = wavf.getframerate()
+
+        total_num_frames = wavf.getnframes()
+        total_chunks = math.ceil(total_num_frames/CHUNK)
+        finalize = False
+
+        for i in range(0, total_chunks):
+            frames = wavf.readframes(CHUNK)
+
+            samples = struct.unpack_from('<%dh' % CHUNK, frames)
+            if i == total_chunks - 1:
+                finalize = True
+
+            if decoder.decode(frame_rate, np.array(samples, dtype=np.float32), finalize):
+                decoded_string, likelihood = decoder.get_decoded_string()
+                #  words, times, lengths = decoder.get_word_alignment()
+                print("**", decoded_string)
+                print("** {} likelihood:".format(model_dir), likelihood)
+            else:
+                raise RuntimeError("Decoding failed")
+
+except RuntimeError:
+    print("***ERROR: Partial decoding of {} failed.".format(wavfile))
