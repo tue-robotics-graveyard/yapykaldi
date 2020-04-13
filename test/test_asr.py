a3aaf4279c720f8a5594552ac59e6e0102559026
Arpit Aggarwal <ar13pit@gmail.com>
2020-04-13 14:40:06 +0200

    style: Remove space in arg
    Co-Authored-By: Matthijs van der Burgh <MatthijsBurgh@outlook.com>

--- test/test_asr.py
+++ test/test_asr.py
@@ -8,7 +8,7 @@ output_dir = "./output"
 
 asr = Asr(model_dir, model_type, output_dir)
 
-def interrupt_handle( sig, frame):
+def interrupt_handle(sig, frame):
     """Interrupt handler that sets the flag to stop recognition and close audio stream"""
     asr.stop()
     time.sleep(3)
