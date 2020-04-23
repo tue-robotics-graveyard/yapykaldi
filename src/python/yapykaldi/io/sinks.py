ab9c6a5266c0abfa7178f086b2b8f1eb901fef3c
Arpit Aggarwal <ar13pit@gmail.com>
2020-04-23 20:31:03 +0200

    feat: Make WaveFileSink inherit from AudioSinkBase

--- src/python/yapykaldi/io/sinks.py
+++ src/python/yapykaldi/io/sinks.py
@@ -45,25 +45,22 @@ class AudioSinkBase(object):
         raise NotImplementedError()
 
 
-class WaveFileSink(object):
-    """WaveFileSink class"""
-
-    def __init__(self, wavpath, fmt=pyaudio.paInt16, channels=1, rate=16000, chunk=1024):
+class WaveFileSink(AudioSinkBase):
+    def __init__(self, wavpath, fmt=pyaudio.paInt16, channels=1, rate=16000, chunksize=1024):
         """
 
         :param wavpath: location where to save audio to
         :param fmt: (default pyaudio.paInt16) Data type of the audio stream
         :param channels: (default 1) Number of channels of the audio stream
         :param rate: (default 16000) Sampling frequency of the audio stream
-        :param chunk: (default 1024) Size of the audio stream buffer
+        :param chunksize: (default 1024) Size of the audio stream buffer
         """
+        super().__init__(rate=rate, chunksize=chunksize, fmt=fmt)
         self._pyaudio = pyaudio.PyAudio()
         self.wavpath = wavpath
-        self.format = fmt
         self.channels = channels
-        self.rate = rate
-        self.chunk = chunk
 
+        self._wavf = None
         self.frames = []
 
     def add_chunk(self, frames):
