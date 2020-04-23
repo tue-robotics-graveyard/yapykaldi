ff519e14fc9812bfd40b775d58b9684154901bb3
Arpit Aggarwal <ar13pit@gmail.com>
2020-04-24 00:24:04 +0200

    feat: Add volume indicator function
    Useful in debugging chunk volume levels

--- src/python/yapykaldi/utils.py
+++ src/python/yapykaldi/utils.py
@@ -1,6 +1,7 @@
 "Common utilities used by other modules"
 import os
 import errno
+import numpy as np
 
 
 def makedir_exist_ok(dirpath):
@@ -16,3 +17,15 @@ def makedir_exist_ok(dirpath):
             pass
         else:
             raise
+
+
+def volume_indicator(data, bitsize=16, bars=79):
+    """Volume level indicator
+
+    :param data: Audio data as a numpy array
+    :param bitsize: (default 16) Bit size of the audio data
+    :param bars: (default 79) Number of bars to display in volume indicators
+    """
+    peak = np.abs(np.max(data) - np.min(data))/2**bitsize
+    volume_level_string = "[" + "#"*int(peak*bars) + "-"*int(bars - peak*bars) + "]"
+    return volume_level_string
