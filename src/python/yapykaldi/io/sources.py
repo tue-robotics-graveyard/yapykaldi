9cd803bb4d999d59b817a58f7be8ebb406075f2b
Arpit Aggarwal <ar13pit@gmail.com>
2020-05-17 00:28:32 +0200

    feat: Change base class to AsrPipelineElementBase

--- src/python/yapykaldi/io/sources.py
+++ src/python/yapykaldi/io/sources.py
@@ -8,6 +8,7 @@ from queue import Empty, Queue
 import pyaudio
 
 from .sinks import WaveFileSink
+from ._base import AsrPipelineElementBase
 from ..logger import logger
 
 try:
@@ -16,47 +17,7 @@ except ImportError:
     pass
 
 
-class AudioSourceBase(object):
-    """The AudioSource
-    It requires some setup before we can get audio bytes from it and
-    requires some teardown afterwards
-
-    The right order is:
-    1. source = AudioSourceBase()
-    2. source.open()                # to open the file, connect the mic etc.
-    3. source.start()               # actually start getting audio data
-    4. source.get_next_chunk()      # use the audio data
-    5. source.stop()                # stop getting audio data
-    6. source.close()               # close the file
-
-    Some sources only support opening them once but
-        they should all support going through start, get.., stop
-        several times
-
-    """
-    # pylint: disable=useless-object-inheritance
-
-    def __init__(self, rate=16000, chunksize=1024):
-        self.rate = rate
-        self.chunksize = chunksize
-
-    def open(self):
-        raise NotImplementedError()
-
-    def start(self):
-        raise NotImplementedError()
-
-    def stop(self):
-        raise NotImplementedError()
-
-    def close(self):
-        raise NotImplementedError()
-
-    def get_next_chunk(self, timeout):
-        raise NotImplementedError()
-
-
-class PyAudioMicrophoneSource(AudioSourceBase):
+class PyAudioMicrophoneSource(AsrPipelineElementBase):
     def __init__(self, fmt=pyaudio.paInt16, channels=1, rate=16000, chunksize=1024, saver=None):
         """
         :param fmt: (default pyaudio.paInt16) format of the audio data
@@ -107,12 +68,12 @@ class PyAudioMicrophoneSource(AudioSourceBase):
         stream.close()
         logger.info("Stopped streaming audio")
 
-    def get_next_chunk(self, timeout=1):
+    def next_chunk(self, timeout=1):
         try:
             # logger.debug("{}\t-1 chunks in the queue".format(self._queue.qsize()))
             chunk = self._queue.get(block=True, timeout=timeout)
             if self.saver:
-                self.saver.add_chunk(chunk)
+                self.saver.next_chunk(chunk=chunk)
             return chunk
         except Empty:
             raise StopIteration()
@@ -128,13 +89,14 @@ class PyAudioMicrophoneSource(AudioSourceBase):
             logger.info("No running audio stream to stop")
 
     def close(self):
+        self.stream.close()
         self._pyaudio.terminate()
 
         if self.saver:
             self.saver.write_frames()
 
 
-class WaveFileSource(AudioSourceBase):
+class WaveFileSource(AsrPipelineElementBase):
     def __init__(self, filename, rate=16000, chunksize=1024):
         """
         :param filename: path to the wave file
@@ -165,7 +127,7 @@ class WaveFileSource(AudioSourceBase):
         self.total_chunks = math.floor(self.total_num_frames / self.chunksize)
         self.read_chunks = 0
 
-    def get_next_chunk(self, timeout):
+    def next_chunk(self):
         if self.read_chunks < self.total_chunks:
             frames = self.wavf.readframes(self.chunksize)
             self.read_chunks += 1
