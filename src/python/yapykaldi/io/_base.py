2e47aba65ad5482953ae84b934dea41b08f80922
Arpit Aggarwal <ar13pit@gmail.com>
2020-05-16 17:41:25 +0200

    feat: Add a common base class definition

---
+++ src/python/yapykaldi/io/_base.py
@@ -0,0 +1,41 @@
+"""Base classes for the ASR pipeline"""
+import pyaudio
+
+
+class AsrPipelineElementBase(object):
+    """Class AsrPipelineElementBase is the base class for all Asr Pipeline elements.
+
+    The right order of setting up an element is:
+    1. element = AsrPipelineElementBase()
+    2. element.open()                # To open the file, connect the mic etc.
+    3. element.start()               # Start streaming audio data
+    4. element.get_chunk()           # Use the audio data
+    5. element.stop()                # stop getting audio data
+    6. element.close()               # close the file
+
+    Element need to support open and close at least once but must support
+    start, get_chunk, stop several times
+
+    """
+    # pylint: disable=useless-object-inheritance
+
+    def __init__(self, rate=16000, chunksize=1024, fmt=pyaudio.paInt16, channels=1):
+        self.rate = rate
+        self.chunksize = chunksize
+        self.format = fmt
+        self.channels = channels
+
+    def open(self):
+        raise NotImplementedError()
+
+    def start(self):
+        raise NotImplementedError()
+
+    def next_chunk(self):
+        raise NotImplementedError()
+
+    def stop(self):
+        raise NotImplementedError()
+
+    def close(self):
+        raise NotImplementedError()
